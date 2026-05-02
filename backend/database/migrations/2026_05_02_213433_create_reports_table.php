7539dead1284bd594a6aab15705b6c7356b392bf
Bazzty <bastiancontreras@backupcode.cl>
2026-05-02 17:54:10 -0400

    feat:Se agrego migracion de reportes para base de datos

---
+++ backend/database/migrations/2026_05_02_213433_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); #  El usuario debe estar registrado para poder hacer un reporte y si el usuario se elimina, también se eliminarán sus reportes.
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->text('description');
+            $table->latitude('latitude', 10, 8);
+            $table->longitude('longitude', 10, 8);
+            $table->string('photo_path');
+            $table->enum('status', ['Pendiente', 'En Progreso', 'Resuelto'])->default('pendiente');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
+      
