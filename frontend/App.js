1120829c3adc3c0d0a09c1b9cb2f974bacfde00c
Oskar Rehbein <orehbeinp@correo.uss.cl>
2026-06-08 21:04:21 -0400

    fix: modificado App.js para que comience en HomeScreen y cordenadas de MapScreen para mostrar Puerto Varas

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import WelcomeScreen from './src/screens/WelcomeScreen';
+// Comenté WelcomeScreen porque no lo agregaste al Stack abajo. 
+// import WelcomeScreen from './src/screens/WelcomeScreen';
+
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -14,10 +16,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Iniciar Sesión' }} />
         <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Registro' }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Mis Reportes' }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio' }} />
         <Stack.Screen name="Map" component={MapScreen} options={{ title: 'Mapa' }} />
         <Stack.Screen name="NewReport" component={NewReportScreen} options={{ title: 'Nuevo Reporte' }} />
         <Stack.Screen name="MyReports" component={MyReportsScreen} options={{ title: 'Mis Reportes' }} />
