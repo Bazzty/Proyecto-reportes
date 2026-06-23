0fa9af4f84356f44b4c8272ef1573859fe728781
mathias <mcarrerah@correo.uss.cl>
2026-06-22 23:16:21 -0400

    correcion PR5

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -40,10 +40,12 @@ class User extends Authenticatable
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
-     */    public function reports()
+     */
+    public function reports()
     {
         return $this->hasMany(Report::class);
     }
+
     protected function casts(): array
     {
         return [
